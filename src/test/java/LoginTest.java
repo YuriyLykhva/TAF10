72f4aa987dad2683d4bc293014415d87e4bd2fdb
Yurii_Lykhva <yuriy.lykhva@gmail.com>
2023-05-05 11:29:35 +0300

    Added Data Provider using csv-file

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,8 +1,14 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import page.SignInPage;
 import util.User;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 public class LoginTest extends BaseTest{
     @Test
@@ -40,5 +46,32 @@ public class LoginTest extends BaseTest{
                 .substring(loggedInUserName.indexOf(" ") + 1, loggedInUserName.indexOf("!"));
 
         Assert.assertEquals(editedLoggedInUserName, userName);
+    }
+
+    @DataProvider(name = "csvCredentials")
+    public Object[][] csvCredentials() throws IOException, CsvException {
+        CSVReader csvReader = new CSVReader(new FileReader("src/main/resources/File.csv"));
+        List<String[]> users = csvReader.readAll();
+
+        String[][] csvDataObject = new String[users.size()][3];
+        for (int i = 0; i < users.size(); i++) {
+            csvDataObject[i] = users.get(i);
+        }
+        return csvDataObject;
+    }
+
+    @Test(enabled = true, description = "Test login with email and password from CSV file", dataProvider = "csvCredentials")
+    public void loginViaCsvFile(String email, String password, String userName) {
+        String loggedInUserName =
+                new SignInPage(driver)
+                        .openPage()
+                        .loginViaCredentials(email, password)
+                        .getLoggedInUserName();
+
+        String editedLoggedInUserName = loggedInUserName
+                .substring(loggedInUserName.indexOf(" ") + 1, loggedInUserName.indexOf("!"));
+
+        Assert.assertEquals(editedLoggedInUserName, userName);
+
     }
 }
